61cb3d86f5b38073d732d3b55f45035d65bfd256
Niladri Kal <niladri.kal@gmail.com>
2022-11-23 14:54:08 -0600

    create the skeleton of the function for slice sampling for surrogate data model

---
+++ R/SurrogateSS.R
@@ -0,0 +1,17 @@
+# This is implementation of algorithm 4 in Adams and Murray(2010)
+
+#' Surrogate data model - Slice sampling
+#'
+#' @param theta hyperparameter of interest
+#' @param f latent variable
+#' @param sigma scale parameter
+#' @param Sig covariance matrix of distribution of f
+#' @param S covariance matrix of the noisy version of the true latent variable
+#'
+#' @return updated theta and f
+#' @export
+#'
+#' @examples
+SurrogateSS <- function(theta, f, sigma, Sig, S){
+
+}
