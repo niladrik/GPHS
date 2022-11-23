215267d64e3f238f3c16568299700c7c324cabd3
Niladri Kal <niladri.kal@gmail.com>
2022-11-23 13:45:10 -0600

    create the skeleton of function for MH algoithm in surrogate data model

---
+++ R/SurrogateMH.R
@@ -0,0 +1,18 @@
+## This is the implementation of Surrogate data Metropolis Hastings algorithm as given in Adams and Murray (2010)
+#
+#' Surrogate data model - Metropolis Hastings
+#'
+#' @param theta hyperparameter of interest
+#' @param f latent variable
+#' @param q proposal distribution
+#' @param Sig covariance matrix of distribution of f
+#' @param S covariance matrix of the noisy version of the true latent variable
+#'
+#' @return updated theta and f
+#' @export
+#'
+#' @examples
+SurrogateMH <- function(theta, f, q, Sig, S){
+
+}
+
