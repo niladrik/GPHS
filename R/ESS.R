8db4618af345ca2b9fdc07d843831d2899376a96
Niladri Kal <niladri.kal@gmail.com>
2022-12-05 15:58:27 -0600

    making provision to include maximum number of iterations

--- R/ESS.R
+++ R/ESS.R
@@ -9,7 +9,7 @@
 #' @param sigma sd of Gaussian process
 #' @param log.L log-likelihood function
 #'
-#' @return new state x'
+#' @return It will return the new state x'
 #' @export
 #'
 #' @examples
@@ -17,7 +17,7 @@
 #'
 #'
 #'
-ESS <- function(x, mu, sigma, log.L){
+ESS <- function(x, mu, sigma, log.L, niter){
 
   # choose ellipse
   v <- mvrnorm(1, mu, sigma)
@@ -36,24 +36,37 @@ ESS <- function(x, mu, sigma, log.L){
   # defining a flag variable for the while loop
   flag <- 1
 
+  # counter
+  count = 0
+
   # slice sampling the loop
   while(flag){
+
+    # counting the number of iterations
+    count = count + 1
 
     # calculating a new x in the proposed angle
     x.n = (x - mu) * cos(theta) + (v - mu) * sin(theta) + mu
 
     # checking if the proposed x is in the slice
     if(log.L(x.n) > log.y){
-      flag <- 0
+      flag = 0
       return(x.n)
     }
 
     # shrinking the bracket and proposing a new angle
     if(theta < 0){
-      theta.min <- theta
+      theta.min = theta
     } else{
-      theta.max <- theta
+      theta.max = theta
     }
-    theta <- runif(1, theta.min, theta.max)
+    theta = runif(1, theta.min, theta.max)
+
+    # checking the number of iterations till now
+    if(count >= niter){
+      print("Exceeded the maximum number of iterations!")
+      flag = 0
+      return(x.n)
+    }
   }
 }
