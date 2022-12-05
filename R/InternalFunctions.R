f99454cede08b637a7d131b4ed6e280fd849e7b2
Niladri Kal <niladri.kal@gmail.com>
2022-12-04 23:54:32 -0600

    create a function to calculate the density of multivariate normal distribution

--- R/InternalFunctions.R
+++ R/InternalFunctions.R
@@ -5,3 +5,13 @@ matern = function(d , phi, nu = 3){ # previous value of nu = 3
            besselK(x = d * phi * sqrt(2 * nu), nu = nu), 1.0)
 }
 
+
+# density of multivariate normal distribution
+dmvnorm <- function(y, mu, sigma){
+  p = length(y)
+  # inverse and det of sigma
+  result = inv_and_logdet(sigma)
+  return(exp(-tcrossprod(y - mu, result[[1]] * (y - mu)) * 0.5 - result[[2]] - p * log(2*pi) / 2))
+}
+
+
