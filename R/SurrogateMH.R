e7a2837a5dbc5c4e0057c64fc6696307ebdb7d69
Niladri Kal <niladri.kal@gmail.com>
2022-12-09 00:09:50 -0600

    fixed a bug regarding the update of the matrices

--- R/SurrogateMH.R
+++ R/SurrogateMH.R
@@ -3,9 +3,10 @@
 #' Surrogate data model - Metropolis Hastings
 #'
 #' @param theta hyperparameter of interest
+#' @param f latent variable
 #' @param data the data in hand
 #' @param p the prior distribution
-#' @param f latent variable
+#' @param l the conditional likelihoof function of data conditioned on the latent variable f
 #'
 #' @return updated theta and f
 #' @export
@@ -14,7 +15,7 @@
 #'
 #'
 #'
-SurrogateMH <- function(theta, f, data, p){
+SurrogateMH <- function(theta, f, data, p, l){
   # fixing S by hand to a constant
   alpha = 0.1
   S = alpha * diag(1, nrow = length(f))
@@ -35,7 +36,7 @@ SurrogateMH <- function(theta, f, data, p){
   R = solve(S_inv + Sig_inv)
 
   # calculating m
-  m = R %*% solve(S) %*% g
+  m = R %*% S_inv %*% g
 
   # Cholesky decomposition of R
   L = chol(R)
@@ -50,22 +51,26 @@ SurrogateMH <- function(theta, f, data, p){
   # taking log-normal(log(theta), 1) distribution to be the proposal distribution
   theta.p = exp(rnorm(1, log(theta), 1))
 
-  # if(theta >= 1){
-  #   # taking the proposal distribuion to be Uniform(theta-1, theta+1)
-  #   theta.p = runif(1, theta - 1, theta + 1)
-  # }else{
-  #   theta.p = 1
-  # }
+  # compute the new Sigma matrix
+  Sigma.p = Sigma(data, theta.p)
+
+  # new R
+  R.p = solve(solve(Sigma.p) + S_inv)
+
+  # calculating new m
+  m.p = R.p %*% S_inv %*% g
+
+  # compute new L matrix
+  L.p = chol(R.p)
 
   # compute the function
-  f.p = as.vector(L %*% eta + m)
+  f.p = as.vector(L.p %*% eta + m.p)
+
+  # generating the threshold randomly
   u = runif(1)
 
-  # calculating new Sigma
-  Sig.p = Sigma(data, theta.p)
-
   # calculate the ratio
-  lik.new = l(f.p) * dmvnorm(g, rep(0, length(g)), Sig.p + S) * p(theta.p) * dnorm(log(theta.p), log(theta), 1) / theta.p
+  lik.new = l(f.p) * dmvnorm(g, rep(0, length(g)), Sigma.p + S) * p(theta.p) * dnorm(log(theta.p), log(theta), 1) / theta.p
   lik.old = l(f) * dmvnorm(g, rep(0, length(g)), Sig + S) * p(theta) * dnorm(log(theta), log(theta.p), 1) / theta
   if(lik.new / lik.old > u){
     return(list(f = f.p, theta = theta.p))
